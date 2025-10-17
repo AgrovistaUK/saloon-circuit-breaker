786ce211c96a24d769426a9434d8e8be865d1235
Tom Hanstead <tom.hanstead@agrovista.co.uk>
2025-10-17 15:41:31 +0100

    Only load Redis when actually needed

--- src/Services/CircuitBreakerRedisRegistry.php
+++ src/Services/CircuitBreakerRedisRegistry.php
@@ -9,27 +9,35 @@ use Illuminate\Support\Facades\Redis;
 
 class CircuitBreakerRedisRegistry
 {
-    private Connection $redis;
+    private ?Connection $redis = null;
     private string $registryKey = CircuitBreakerCacheEnum::ACTIVE_SERVICES->value;
 
-    public function __construct() {
-        $this->redis = Redis::connection();
+    private function redis(): Connection
+    {
+        if (!$this->redis) {
+            $this->redis = Redis::connection();
+        }
+        return $this->redis;
     }
 
-    public function registerService(string $service): void{
-        $this->redis->sadd($this->registryKey, $service);
-        $this->redis->expire($this->registryKey, 86400);
+    public function registerService(string $service): void
+    {
+        $this->redis()->sadd($this->registryKey, $service);
+        $this->redis()->expire($this->registryKey, 86400);
     }
 
-    public function getActiveServices(): array {
-        return $this->redis->smembers($this->registryKey);
+    public function getActiveServices(): array
+    {
+        return $this->redis()->smembers($this->registryKey);
     }
 
-    public function isServiceRegistered(string $service): bool {
-        return $this->redis->sismember($this->registryKey, $service);
+    public function isServiceRegistered(string $service): bool
+    {
+        return $this->redis()->sismember($this->registryKey, $service);
     }
 
-    public function unregisterService(string $service): void {
-        $this->redis->srem($this->registryKey, $service);
+    public function unregisterService(string $service): void
+    {
+        $this->redis()->srem($this->registryKey, $service);
     }
 }
